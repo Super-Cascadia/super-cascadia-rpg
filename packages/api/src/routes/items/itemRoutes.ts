9e3eca7a0dd0c278833d058115eeb7529b0b1293
wintermuted <jameson.nyeholt@gmail.com>
2020-02-19 08:53:44 -0800

    get item by id

--- packages/api/src/routes/items/itemRoutes.ts
+++ packages/api/src/routes/items/itemRoutes.ts
@@ -1,26 +1,43 @@
 import {Request, ResponseToolkit, Server} from "@hapi/hapi";
 import {Connection} from "typeorm";
 import {ItemModel} from '../../model/items/itemModel';
-import { map } from 'lodash'
-import { Item } from '../../db/entity/Item';
+import {map} from 'lodash'
+import {Item} from '../../db/entity/Item';
+
+function mapItemtoItemModel(items: Item[]): ItemModel[] {
+    return map(items, (item: Item) => {
+        return {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            type: item.type
+        } as ItemModel
+    });
+}
+
+async function getAllItems(connection: Connection): Promise<ItemModel[]> {
+    const items = await connection.manager.find(Item);
+
+    return mapItemtoItemModel(items);
+}
+
+async function getItemById(connection: Connection, id: string): Promise<ItemModel[]> {
+    const items = await connection.manager.findByIds(Item, [id]);
+
+    return mapItemtoItemModel(items)
+}
 
 export const itemRoutes = (server: Server, connection: Connection) => {
     server.route({
         method: 'GET',
-        path: '/items',
+        path: '/items/{id?}',
         handler: async (request: Request, reply: ResponseToolkit): Promise<ItemModel[]> => {
-            let items = await connection.manager.find(Item);
 
-            const itemModels: ItemModel[] = map(items, (item: Item) => {
-                return {
-                    id: item.id,
-                    name: item.name,
-                    description: item.description,
-                    type: item.type
-                } as ItemModel
-            });
-
-            return Promise.resolve(itemModels);
+            if (request.params.id) {
+                return Promise.resolve(await getItemById(connection, request.params.id));
+            } else {
+                return Promise.resolve(await getAllItems(connection));
+            }
         }
     });
 };
