271c3a1b6463353a36a28dfb9c29e723aaf38c95
wintermuted <jameson.nyeholt@gmail.com>
2020-11-04 22:04:06 -0800

    move form to local component

--- packages/ui/src/pages/Items/ItemEdit.tsx
+++ packages/ui/src/pages/Items/ItemEdit.tsx
@@ -11,6 +11,72 @@ import { Item } from "@super-cascadia-rpg/api";
 import Loading from "../../components/Loading";
 import { ObjectDetailEditPageWrapper } from "../../components/ObjectDetailEditPageWrapper";
 
+function ItemEditForm({
+  item,
+  handleFormChange,
+}: {
+  item: Item;
+  handleFormChange: (event: React.SyntheticEvent) => void;
+}) {
+  return (
+    <>
+      <Form.Group as={Row} controlId="formId">
+        <Form.Label column sm="2">
+          ID
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control readOnly as="input" value={toString(item.id)} />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} controlId="name">
+        <Form.Label column sm="2">
+          Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control
+            as="input"
+            value={item.name}
+            onChange={(e: SyntheticEvent) => handleFormChange(e)}
+          />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} controlId="description">
+        <Form.Label column sm="2">
+          Description
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control
+            as="input"
+            value={item.description}
+            onChange={(e: SyntheticEvent) => handleFormChange(e)}
+          />
+        </Col>
+      </Form.Group>
+
+      <Form.Group
+        as={Row}
+        controlId="type"
+        onChange={(e: SyntheticEvent) => handleFormChange(e)}
+      >
+        <Form.Label column sm="2">
+          Item Type
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control as="select" custom value={toString(item.type)}>
+            <option value={0}>Food</option>
+            <option value={1}>Weapon</option>
+            <option value={2}>Armor</option>
+            <option value={3}>Accessory</option>
+            <option value={4}>Key Item</option>
+          </Form.Control>
+        </Col>
+      </Form.Group>
+    </>
+  );
+}
+
 export default function ItemEdit() {
   const { id: itemId } = useParams<{ id: string }>();
   const id = toNumber(itemId);
@@ -32,10 +98,6 @@ export default function ItemEdit() {
   };
 
   const handleSubmit = (event: SyntheticEvent) => {
-    const form = event?.target as HTMLInputElement;
-
-    console.log(form);
-
     event.preventDefault();
     event.stopPropagation();
 
@@ -48,8 +110,6 @@ export default function ItemEdit() {
   // @ts-ignore
   useEffect(fetchItemDataHook(id, setData), {});
 
-  console.log(data);
-
   if (isEmpty(item)) {
     return <Loading />;
   }
@@ -61,61 +121,7 @@ export default function ItemEdit() {
       routeName={"items"}
       handleSubmit={handleSubmit}
     >
-      <>
-        <Form.Group as={Row} controlId="formId">
-          <Form.Label column sm="2">
-            ID
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control readOnly as="input" value={toString(item.id)} />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} controlId="name">
-          <Form.Label column sm="2">
-            Name
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control
-              as="input"
-              value={item.name}
-              onChange={(e: SyntheticEvent) => handleFormChange(e)}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} controlId="description">
-          <Form.Label column sm="2">
-            Description
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control
-              as="input"
-              value={item.description}
-              onChange={(e: SyntheticEvent) => handleFormChange(e)}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group
-          as={Row}
-          controlId="type"
-          onChange={(e: SyntheticEvent) => handleFormChange(e)}
-        >
-          <Form.Label column sm="2">
-            Item Type
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control as="select" custom value={toString(item.type)}>
-              <option value={0}>Food</option>
-              <option value={1}>Weapon</option>
-              <option value={2}>Armor</option>
-              <option value={3}>Accessory</option>
-              <option value={4}>Key Item</option>
-            </Form.Control>
-          </Col>
-        </Form.Group>
-      </>
+      <ItemEditForm item={item} handleFormChange={handleFormChange} />
     </ObjectDetailEditPageWrapper>
   );
 }
