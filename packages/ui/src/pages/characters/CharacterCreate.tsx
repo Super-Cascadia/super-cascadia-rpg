65aea8f09b9f3c7f2915c9c8da6f673d8b4f9dcd
wintermuted <jameson.nyeholt@gmail.com>
2020-11-05 17:16:18 -0800

    migrate CharacterCreate.tsx to use Object CreatePageWrapper

--- packages/ui/src/pages/characters/CharacterCreate.tsx
+++ packages/ui/src/pages/characters/CharacterCreate.tsx
@@ -1,19 +1,77 @@
-import React, { useState, SyntheticEvent } from "react";
-import Container from "react-bootstrap/Container";
+import React, { SyntheticEvent, useState } from "react";
 import Form from "react-bootstrap/Form";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import { useHistory } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { CharacterClassId } from "@super-cascadia-rpg/api/build/src/model/characterClass/characterClassModel";
 import createCharacter from "../../api/characters/createCharacter";
+import { ObjectCreatePageWrapper } from "../../components/ObjectCreatePageWrapper";
 
 const initialFormState = {
   name: "",
   description: "",
   type: CharacterClassId.FREELANCER,
 };
+
+function NewComponent({
+  handleFormChange,
+}: {
+  handleFormChange: (e: React.SyntheticEvent) => void;
+}) {
+  return (
+    <>
+      <Form.Group as={Row} controlId="firstName">
+        <Form.Label column sm="2">
+          First Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control as="input" onChange={handleFormChange} />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} controlId="lastName">
+        <Form.Label column sm="2">
+          Last Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control as="input" onChange={handleFormChange} />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} controlId="description">
+        <Form.Label column sm="2">
+          Description
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control onChange={handleFormChange} />
+          <Form.Text className="text-muted">
+            a description of the character
+          </Form.Text>
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} controlId="primaryClass" onChange={handleFormChange}>
+        <Form.Label column sm="2">
+          Primary Class
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control as="select">
+            <option value={0}>Freelancer</option>
+            <option value={1}>Rogue</option>
+            <option value={2}>Warrior</option>
+            <option value={3}>Mage</option>
+            <option value={4}>Druid</option>
+            <option value={5}>Sorcerer</option>
+          </Form.Control>
+          <Form.Text className="text-muted">
+            The Primary class of the character. Determines key attributes and
+            modifiers.
+          </Form.Text>
+        </Col>
+      </Form.Group>
+    </>
+  );
+}
 
 export default function CharacterCreate() {
   const [formState, updateFormState] = useState(initialFormState);
@@ -41,83 +99,11 @@ export default function CharacterCreate() {
   };
 
   return (
-    <Container>
-      <br />
-      <Card>
-        <Form onSubmit={handleSubmit}>
-          <Card.Header>Create a new Character</Card.Header>
-          <Card.Body>
-            <Form.Group as={Row} controlId="firstName">
-              <Form.Label column sm="2">
-                First Name
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control
-                  as="input"
-                  onChange={(e: SyntheticEvent) => handleFormChange(e)}
-                />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} controlId="lastName">
-              <Form.Label column sm="2">
-                Last Name
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control
-                  as="input"
-                  onChange={(e: SyntheticEvent) => handleFormChange(e)}
-                />
-              </Col>
-            </Form.Group>
-
-            <Form.Group as={Row} controlId="description">
-              <Form.Label column sm="2">
-                Description
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control
-                  onChange={(e: SyntheticEvent) => handleFormChange(e)}
-                />
-                <Form.Text className="text-muted">
-                  a description of the character
-                </Form.Text>
-              </Col>
-            </Form.Group>
-
-            <Form.Group
-              as={Row}
-              controlId="primaryClass"
-              onChange={(e: SyntheticEvent) => handleFormChange(e)}
-            >
-              <Form.Label column sm="2">
-                Primary Class
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control as="select">
-                  <option value={0}>Freelancer</option>
-                  <option value={1}>Rogue</option>
-                  <option value={2}>Warrior</option>
-                  <option value={3}>Mage</option>
-                  <option value={4}>Druid</option>
-                  <option value={5}>Sorcerer</option>
-                </Form.Control>
-                <Form.Text className="text-muted">
-                  The Primary class of the character. Determines key attributes
-                  and modifiers.
-                </Form.Text>
-              </Col>
-            </Form.Group>
-          </Card.Body>
-          <Card.Footer className="text-muted">
-            <Form.Group role="form">
-              <Button variant="primary" size="sm" type="submit">
-                Submit
-              </Button>
-            </Form.Group>
-          </Card.Footer>
-        </Form>
-      </Card>
-    </Container>
+    <ObjectCreatePageWrapper
+      name={"Create new Character"}
+      handleSubmit={handleSubmit}
+    >
+      <NewComponent handleFormChange={handleFormChange} />
+    </ObjectCreatePageWrapper>
   );
 }
