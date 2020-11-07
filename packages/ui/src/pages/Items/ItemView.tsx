074d7134fbf7e403fb99a87a6bc2a0856d729534
wintermuted <jameson.nyeholt@gmail.com>
2020-11-07 13:24:21 -0800

    migrate ItemView.tsx to generic inputs

--- packages/ui/src/pages/Items/ItemView.tsx
+++ packages/ui/src/pages/Items/ItemView.tsx
@@ -6,56 +6,43 @@ import Form from "react-bootstrap/Form";
 import { useParams } from "react-router-dom";
 import fetchItemDataHook from "../../hooks/api/items/fetchItemDataHook";
 import { getItemTypeNameById } from "../../util/itemType";
-import { isEmpty, toNumber } from "lodash";
+import { isEmpty, toNumber, toString } from "lodash";
 import Loading from "../../components/Loading";
 import ObjectDetailViewPageWrapper from "../../components/ObjectDetailViewPageWrapper";
+import { TextInput } from "../../components/forms/TextInput";
+import { itemTypeOptions } from "./constants";
+import { SelectInput } from "../../components/forms/SelectInput";
 
 function ItemViewForm({ item }: { item: ItemModel }) {
   const itemTypeName = getItemTypeNameById(item?.type);
 
   return (
     <>
-      <Form.Group as={Row} controlId="formId">
-        <Form.Label column sm="2">
-          ID
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control readOnly defaultValue={item.id} />
-        </Col>
-      </Form.Group>
+      <TextInput label="ID" id="id" readOnly defaultValue={item.id} />
 
-      <Form.Group as={Row} controlId="formName">
-        <Form.Label column sm="2">
-          Name
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control readOnly defaultValue={item.name} />
-        </Col>
-      </Form.Group>
+      <TextInput
+        label="Name"
+        id="name"
+        readOnly
+        defaultValue={item.name}
+        inputDescription="The name of the item."
+      />
 
-      <Form.Group as={Row} controlId="formDescription">
-        <Form.Label column sm="2">
-          Description
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control readOnly value={item.description} />
-        </Col>
-      </Form.Group>
+      <TextInput
+        label={"Description"}
+        id={"description"}
+        inputDescription="a description of the item"
+        readOnly
+        value={item.description}
+      />
 
-      <Form.Group as={Row} controlId="formType">
-        <Form.Label column sm="2">
-          Type
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control as="select" readOnly value={itemTypeName}>
-            <option value={0}>Food</option>
-            <option value={1}>Weapon</option>
-            <option value={2}>Accessory</option>
-            <option value={3}>Key Item</option>
-            <option value={4}>Armor</option>
-          </Form.Control>
-        </Col>
-      </Form.Group>
+      <SelectInput
+        label="Item Type"
+        id="type"
+        options={itemTypeOptions}
+        value={itemTypeName}
+        inputDescription="The classification of the item."
+      />
     </>
   );
 }
