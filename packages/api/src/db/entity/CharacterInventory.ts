d2d0786d31bc6b79b8b94d3653fc4dae4ee90cd7
wintermuted <jameson.nyeholt@gmail.com>
2020-11-14 09:13:25 -0800

    stub out character inventory

--- packages/api/src/db/entity/CharacterInventory.ts
+++ packages/api/src/db/entity/CharacterInventory.ts
@@ -1,10 +1,4 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  ManyToOne,
-  OneToOne,
-  JoinColumn,
-} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 import { Character } from "./Character";
 import { Item } from "./Item";
 
@@ -16,4 +10,8 @@ export class CharacterInventory {
   @ManyToOne(() => Character, (character) => character.items)
   // @ts-ignore
   character: Character;
+
+  @ManyToOne(() => Item, (item) => item.characterInventory)
+  // @ts-ignore
+  item: Item;
 }
--- packages/api/src/db/entity/Item.ts
+++ packages/api/src/db/entity/Item.ts
@@ -1,18 +1,25 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
-import { ItemType } from './interfaces/items';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { ItemType } from "./interfaces/items";
+import { CharacterInventory } from "./CharacterInventory";
 
 @Entity()
 export class Item {
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @Column()
+  name!: string;
 
-    @Column()
-    name!: string;
+  @Column()
+  description!: string;
 
-    @Column()
-    description!: string;
+  @Column()
+  type!: ItemType;
 
-    @Column()
-    type!: ItemType;
+  @OneToMany(
+    () => CharacterInventory,
+    (characterInventory) => characterInventory.item
+  )
+  // @ts-ignore
+  characterInventory: CharacterInventory[];
 }
