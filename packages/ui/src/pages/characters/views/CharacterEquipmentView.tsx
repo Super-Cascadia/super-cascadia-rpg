955793fac5aebab8132262c61cfe30347e8864e2
wintermuted <jameson.nyeholt@gmail.com>
2020-11-23 08:34:45 -0800

    update view layout

--- packages/ui/src/pages/characters/views/CharacterEquipmentView.tsx
+++ packages/ui/src/pages/characters/views/CharacterEquipmentView.tsx
@@ -1,8 +1,11 @@
-import { CharacterWithAttributes } from "@super-cascadia-rpg/api";
+import {
+  CharacterInventory,
+  CharacterWithAttributes,
+} from "@super-cascadia-rpg/api";
 import Container from "react-bootstrap/Container";
 import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
-import { toNumber } from "lodash";
+import { toNumber, isUndefined, isNull } from "lodash";
 import { CharacterEquipmentStateHook } from "../../../hooks/store/characterStateHooks";
 import fetchCharacterEquipmentDataHook from "../../../hooks/api/characters/fetchCharacterEquipmentDataHook";
 import Card from "react-bootstrap/Card";
@@ -16,31 +19,41 @@ interface Props {
 
 function EquipmentLocation({
   headerTitle,
-  name,
-  description,
-  buttonLabel,
   image,
+  item,
 }: {
   headerTitle?: string;
-  name?: string;
-  description?: string;
   buttonLabel?: string;
   image: string;
+  item: CharacterInventory | null;
 }) {
   const [imagePath, setImage] = useState("");
+
+  if (isNull(item) || isUndefined(item)) {
+    return (
+      <Card style={{ marginBottom: "20px" }} bg="light" text="dark">
+        <Card.Header>{headerTitle}</Card.Header>
+        <Card.Img variant="top" src={imagePath} />
+        <Card.Body>
+          <Card.Text>No Item Equipped</Card.Text>
+          <Button variant="primary">Equip Item</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 
   import(
     `../../../images/icons/items/rpg_icons/individual_32x32/${image}`
   ).then((module) => setImage(module.default));
 
   return (
-    <Card style={{ marginBottom: "20px" }}>
+    <Card style={{ marginBottom: "20px" }} bg="secondary" text="light">
       <Card.Header>{headerTitle}</Card.Header>
-      <Card.Img variant="top" src={imagePath} />
+      <Card.Img style={{ width: "50px" }} variant="top" src={imagePath} />
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Button variant="primary">{buttonLabel}</Button>
+        <Card.Title>{item.item.name}</Card.Title>
+        <Card.Text>{item.item.description}</Card.Text>
+        <Button variant="primary">Change Item</Button>
       </Card.Body>
     </Card>
   );
@@ -62,62 +75,56 @@ export default function CharacterEquipmentView({ character }: Props) {
       <br />
       <Row>
         <Col>
-          {equipment.leftHand && (
-            <EquipmentLocation
-              headerTitle="Left Hand"
-              name={equipment.leftHand.item.name}
-              description={equipment.leftHand.item.description}
-              buttonLabel="Change Item"
-              image="icon506.png"
-            />
-          )}
-        </Col>
-        <Col>
-          <EquipmentLocation
-            headerTitle="Right Hand"
-            description={undefined}
-            buttonLabel={undefined}
-            image="icon506.png"
-          />
-        </Col>
-        <Col>
           <EquipmentLocation
             headerTitle="Head"
-            description={undefined}
-            buttonLabel={undefined}
+            image="icon506.png"
+            item={equipment.head}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <EquipmentLocation
+            headerTitle="Left Hand"
+            item={equipment.leftHand}
             image="icon506.png"
           />
         </Col>
         <Col>
           <EquipmentLocation
             headerTitle="Chest"
-            description={undefined}
-            buttonLabel={undefined}
             image="icon506.png"
+            item={equipment.chest}
+          />
+          <EquipmentLocation
+            headerTitle="Arms"
+            image="icon506.png"
+            item={equipment.arms}
           />
         </Col>
         <Col>
           <EquipmentLocation
-            headerTitle="Arms"
-            description={undefined}
-            buttonLabel={undefined}
+            headerTitle="Right Hand"
+            item={equipment.rightHand}
             image="icon506.png"
           />
         </Col>
+      </Row>
+      <Row>
         <Col>
           <EquipmentLocation
             headerTitle="Legs"
-            description={undefined}
-            buttonLabel={undefined}
             image="icon506.png"
+            item={equipment.legs}
           />
         </Col>
+      </Row>
+      <Row>
         <Col>
           <EquipmentLocation
             headerTitle="Feet"
-            description={undefined}
-            buttonLabel={undefined}
             image="icon506.png"
+            item={equipment.feet}
           />
         </Col>
       </Row>
