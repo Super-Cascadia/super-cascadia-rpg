e67003b874924fd15b3a846fd6ad09b2f15d2eb9
wintermuted <jameson.nyeholt@gmail.com>
2020-11-23 08:41:37 -0800

    break out EquipmentLocation.tsx

---
+++ packages/ui/src/pages/characters/components/EquipmentLocation.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { isNull, isUndefined } from "lodash";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { CharacterInventory } from "@super-cascadia-rpg/api";
+
+export default function EquipmentLocation({
+  headerTitle,
+  image,
+  item,
+}: {
+  headerTitle?: string;
+  buttonLabel?: string;
+  image: string;
+  item: CharacterInventory | null;
+}) {
+  const [imagePath, setImage] = useState("");
+
+  if (isNull(item) || isUndefined(item)) {
+    return (
+      <Card style={{ marginBottom: "20px" }} bg="light" text="dark">
+        <Card.Img variant="top" src={imagePath} />
+        <Card.Body>
+          <Button variant="primary">Equip Item</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">{headerTitle}</Card.Footer>
+      </Card>
+    );
+  }
+
+  import(
+    `../../../images/icons/items/rpg_icons/individual_32x32/${image}`
+  ).then((module) => setImage(module.default));
+
+  return (
+    <Card style={{ marginBottom: "20px" }} bg="secondary" text="light">
+      <Card.Img variant="top" src={imagePath} />
+      <Card.Body>
+        <Card.Title>{item.item.name}</Card.Title>
+        <Card.Text>{item.item.description}</Card.Text>
+        <Button variant="primary">Change Item</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted">{headerTitle}</Card.Footer>
+    </Card>
+  );
+}
--- packages/ui/src/pages/characters/views/CharacterEquipmentView.tsx
+++ packages/ui/src/pages/characters/views/CharacterEquipmentView.tsx
@@ -1,62 +1,16 @@
-import {
-  CharacterInventory,
-  CharacterWithAttributes,
-} from "@super-cascadia-rpg/api";
+import { CharacterWithAttributes } from "@super-cascadia-rpg/api";
 import Container from "react-bootstrap/Container";
 import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
-import { toNumber, isUndefined, isNull } from "lodash";
+import { toNumber } from "lodash";
 import { CharacterEquipmentStateHook } from "../../../hooks/store/characterStateHooks";
 import fetchCharacterEquipmentDataHook from "../../../hooks/api/characters/fetchCharacterEquipmentDataHook";
-import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 import { CharacterEquipmentExpanded } from "@super-cascadia-rpg/api/build/src/handlers/characterEquipment/util";
+import EquipmentLocation from "../components/EquipmentLocation";
 
 interface Props {
   character: CharacterWithAttributes;
-}
-
-function EquipmentLocation({
-  headerTitle,
-  image,
-  item,
-}: {
-  headerTitle?: string;
-  buttonLabel?: string;
-  image: string;
-  item: CharacterInventory | null;
-}) {
-  const [imagePath, setImage] = useState("");
-
-  if (isNull(item) || isUndefined(item)) {
-    return (
-      <Card style={{ marginBottom: "20px" }} bg="light" text="dark">
-        <Card.Header>{headerTitle}</Card.Header>
-        <Card.Img variant="top" src={imagePath} />
-        <Card.Body>
-          <Card.Text>No Item Equipped</Card.Text>
-          <Button variant="primary">Equip Item</Button>
-        </Card.Body>
-      </Card>
-    );
-  }
-
-  import(
-    `../../../images/icons/items/rpg_icons/individual_32x32/${image}`
-  ).then((module) => setImage(module.default));
-
-  return (
-    <Card style={{ marginBottom: "20px" }} bg="secondary" text="light">
-      <Card.Header>{headerTitle}</Card.Header>
-      <Card.Img style={{ width: "50px" }} variant="top" src={imagePath} />
-      <Card.Body>
-        <Card.Title>{item.item.name}</Card.Title>
-        <Card.Text>{item.item.description}</Card.Text>
-        <Button variant="primary">Change Item</Button>
-      </Card.Body>
-    </Card>
-  );
 }
 
 export default function CharacterEquipmentView({ character }: Props) {
