8b6e7d93775d6fae95aada0758b682efdc8107ea
wintermuted <jameson.nyeholt@gmail.com>
2020-11-23 21:40:00 -0800

    move form scope

--- packages/ui/src/pages/characters/components/form/ChangeEquipmentForm.tsx
+++ packages/ui/src/pages/characters/components/form/ChangeEquipmentForm.tsx
@@ -1,71 +1,33 @@
-import {
-  CharacterInventoryState,
-  CharacterInventoryStateHook,
-} from "../../../../hooks/store/characterStateHooks";
-import React, { useEffect, useState } from "react";
-import fetchCharacterInventoryDataHook from "../../../../hooks/api/characters/fetchCharacterInventoryDataHook";
+import React from "react";
 import { toNumber, find } from "lodash";
-import { Formik, FormikHelpers, FormikValues } from "formik";
-import { DEFAULT_OPTION_ID } from "./controls/constants";
-import * as yup from "yup";
-import Form from "react-bootstrap/Form";
 import InventorySelectControl from "./controls/InventorySelectControl";
 import EquipmentLocation from "../EquipmentLocation";
+import { CharacterInventory } from "@super-cascadia-rpg/api";
 
-export function ChangeEquipmentForm({ characterId }: { characterId: number }) {
-  const [inventory, setInventory]: CharacterInventoryStateHook = useState(
-    [] as CharacterInventoryState
+export function ChangeEquipmentForm({
+  inventory,
+  itemId,
+  handleChange,
+}: {
+  inventory: CharacterInventory[];
+  itemId: string;
+  handleChange: (event: React.SyntheticEvent) => void;
+}) {
+  const inventoryItem = find(
+    inventory,
+    (inventoryItem) => inventoryItem.id === toNumber(itemId)
   );
-
-  useEffect(
-    fetchCharacterInventoryDataHook(toNumber(characterId), setInventory),
-    // @ts-ignore
-    {}
-  );
-
-  const handleSubmit = (values: FormikValues, actions: FormikHelpers<any>) => {
-    console.log(values);
-
-    if (values.itemId !== DEFAULT_OPTION_ID) {
-      actions.setSubmitting(true);
-    }
-  };
-
-  const initialFormState = {
-    itemId: "",
-  };
-
-  const schema = yup.object({
-    itemId: yup.string(),
-  });
 
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={handleSubmit}
-      initialValues={initialFormState}
-    >
-      {({ handleSubmit, dirty, handleChange, values, touched, errors }) => {
-        const inventoryItem = find(
-          inventory,
-          (inventoryItem) => inventoryItem.id === toNumber(values.itemId)
-        );
-
-        console.log("inventory item", inventoryItem, inventory, values);
-
-        return (
-          <Form onSubmit={handleSubmit} noValidate>
-            <InventorySelectControl
-              inventory={inventory}
-              selectedItem={values.itemId}
-              handleChange={handleChange}
-            />
-            {inventoryItem && (
-              <EquipmentLocation headerTitle="Left Hand" item={inventoryItem} />
-            )}
-          </Form>
-        );
-      }}
-    </Formik>
+    <>
+      <InventorySelectControl
+        inventory={inventory}
+        selectedItem={itemId}
+        handleChange={handleChange}
+      />
+      {inventoryItem && (
+        <EquipmentLocation headerTitle="Left Hand" item={inventoryItem} />
+      )}
+    </>
   );
 }
--- packages/ui/src/pages/characters/components/modal/ChangeEquipmentModal.tsx
+++ packages/ui/src/pages/characters/components/modal/ChangeEquipmentModal.tsx
@@ -1,11 +1,21 @@
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CharacterInventory } from "@super-cascadia-rpg/api";
 import Row from "react-bootstrap/Row";
 import EquipmentLocation from "../EquipmentLocation";
 import Col from "react-bootstrap/Col";
 import { ChangeEquipmentForm } from "../form/ChangeEquipmentForm";
+import { Formik, FormikHelpers, FormikValues } from "formik";
+import * as yup from "yup";
+import {
+  CharacterInventoryState,
+  CharacterInventoryStateHook,
+} from "../../../../hooks/store/characterStateHooks";
+import fetchCharacterInventoryDataHook from "../../../../hooks/api/characters/fetchCharacterInventoryDataHook";
+import { toNumber } from "lodash";
+import { DEFAULT_OPTION_ID } from "../form/controls/constants";
+import Form from "react-bootstrap/Form";
 
 interface Props {
   show: boolean;
@@ -22,39 +32,84 @@ export default function ChangeEquipmentModal({
   equipmentLocation,
   characterId,
 }: Props) {
+  const [inventory, setInventory]: CharacterInventoryStateHook = useState(
+    [] as CharacterInventoryState
+  );
+
+  useEffect(
+    fetchCharacterInventoryDataHook(toNumber(characterId), setInventory),
+    // @ts-ignore
+    {}
+  );
+
+  const handleSubmit = (values: FormikValues, actions: FormikHelpers<any>) => {
+    console.log(values);
+
+    if (values.itemId !== DEFAULT_OPTION_ID) {
+      actions.setSubmitting(true);
+    }
+  };
+
+  const initialFormState = {
+    itemId: "",
+  };
+
+  const schema = yup.object({
+    itemId: yup.string(),
+  });
+
   return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Change Equipment</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Row>
-          <Col>
-            <p>Choose a new item.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3>Current Item</h3>
-            <EquipmentLocation
-              headerTitle={equipmentLocation}
-              item={selectedItem}
-            />
-          </Col>
-          <Col>
-            <h3>New Item</h3>
-            <ChangeEquipmentForm characterId={characterId} />
-          </Col>
-        </Row>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => handleClose()}>
-          Cancel
-        </Button>
-        <Button variant="success" onClick={() => handleClose(selectedItem)}>
-          Change Item
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <Formik
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+      initialValues={initialFormState}
+    >
+      {({ handleSubmit, dirty, handleChange, values, touched, errors }) => {
+        return (
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Change Equipment</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Row>
+                <Col>
+                  <p>Choose a new item.</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <h3>Current Item</h3>
+                  <EquipmentLocation
+                    headerTitle={equipmentLocation}
+                    item={selectedItem}
+                  />
+                </Col>
+                <Col>
+                  <h3>New Item</h3>
+                  <Form onSubmit={handleSubmit} noValidate>
+                    <ChangeEquipmentForm
+                      inventory={inventory}
+                      itemId={values.itemId}
+                      handleChange={handleChange}
+                    />
+                  </Form>
+                </Col>
+              </Row>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => handleClose()}>
+                Cancel
+              </Button>
+              <Button
+                variant="success"
+                onClick={() => handleClose(selectedItem)}
+              >
+                Change Item
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }}
+    </Formik>
   );
 }
