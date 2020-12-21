16e283dda292d4b13cc7adc6c0725b4993b3b918
wintermuted <jameson.nyeholt@gmail.com>
2020-12-21 08:33:11 -0800

    add nav and sub routes

--- packages/ui/src/pages/Items/v2/ItemsPage.tsx
+++ packages/ui/src/pages/Items/v2/ItemsPage.tsx
@@ -1,5 +1,47 @@
 import React, { useState } from "react";
 import GridPageWrapperV2 from "../../../components/pageWrapper/GridPageWrapperV2";
+import Nav from "react-bootstrap/Nav";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+
+function ItemsPageRoutes() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route exact path="/">
+        Base path!
+      </Route>
+      <Route path={`${path}/consumables`}>Consumables!</Route>
+      <Route path={`${path}/armors`}>Armor!</Route>
+      <Route path={`${path}/weapons`}>Weapons!</Route>
+    </Switch>
+  );
+}
+
+function ItemsPageNav() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <Nav variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
+        <LinkContainer to={`${url}/consumables`}>
+          <Nav.Link>Consumables</Nav.Link>
+        </LinkContainer>
+      </Nav.Item>
+      <Nav.Item>
+        <LinkContainer to={`${url}/armors`}>
+          <Nav.Link>Armors</Nav.Link>
+        </LinkContainer>
+      </Nav.Item>
+      <Nav.Item>
+        <LinkContainer to={`${url}/weapons`}>
+          <Nav.Link>Weapon</Nav.Link>
+        </LinkContainer>
+      </Nav.Item>
+    </Nav>
+  );
+}
 
 export default function ItemsPage() {
   const [showCreateItemModal, setCreateItemModalViz] = useState<boolean>(false);
@@ -9,7 +51,10 @@ export default function ItemsPage() {
       handleNewButtonClick={() => setCreateItemModalViz(true)}
       title="Items"
     >
-      <div>Table goes here</div>
+      <>
+        <ItemsPageNav />
+        <ItemsPageRoutes />
+      </>
     </GridPageWrapperV2>
   );
 }
