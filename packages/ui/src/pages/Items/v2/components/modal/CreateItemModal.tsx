fe8953230f5ed1af6f73a9f62fb5dc814250c706
wintermuted <jameson.nyeholt@gmail.com>
2021-01-18 11:24:21 -0800

    switch create modal based on route

--- packages/ui/src/pages/Items/v2/components/modal/CreateItemModal.tsx
+++ packages/ui/src/pages/Items/v2/components/modal/CreateItemModal.tsx
@@ -1,28 +1,17 @@
 import React from "react";
-import { Formik, FormikHelpers, FormikValues } from "formik";
-import * as yup from "yup";
-import BasicModal from "../../../../../components/modal/BasicModal";
-import { CreateItemForm } from "../form/CreateItemForm";
-import { useLocation, useRouteMatch } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { last } from "lodash";
-
-const schema = yup.object({
-  name: yup.string(),
-  description: yup.string(),
-  salvageable: yup.boolean(),
-  baseMonetaryValue: yup.number(),
-  consumable: yup.boolean(),
-  recoversHealth: yup.boolean(),
-  healthRecoveryFactor: yup.string(),
-  recoversMana: yup.boolean(),
-  manaRecoveryFactor: yup.string(),
-  recoversStamina: yup.boolean(),
-  staminaRecoveryFactor: yup.string(),
-});
+import CreateConsumableItemModal from "./CreateConsumableItemModal";
 
 interface Props {
   show: boolean;
   handleClose: () => void;
+}
+
+enum ITEM_TYPE {
+  CONSUMABLES = "consumables",
+  ARMORS = "armors",
+  WEAPONS = "weapons",
 }
 
 export default function CreateItemModal({ show, handleClose }: Props) {
@@ -30,71 +19,20 @@ export default function CreateItemModal({ show, handleClose }: Props) {
   const itemType = last(pathname.split("/"));
   console.log("useLocation", pathname, itemType);
 
-  const handleSubmitSuccess = (actions: FormikHelpers<any>) => () => {
-    actions.resetForm({
-      values: {},
-    });
-    handleClose();
-  };
-
-  const handleSubmit = (values: FormikValues, actions: FormikHelpers<any>) => {
-    console.log(values);
-
-    // actions.setSubmitting(true);
-    //
-    // const itemObject = {
-    //   ...values,
-    // } as BasicConsumableItem;
-    //
-    // updateConsumableItem(itemObject.id, itemObject).then(
-    //   handleSubmitSuccess(actions)
-    // );
-  };
-
-  const handleChange = (values: FormikValues, actions: FormikHelpers<any>) => {
-    console.log(values);
-  };
-
-  const initialFormState = {
-    name: "",
-    description: "",
-    salvageable: false,
-    baseMonetaryValue: 0,
-    consumable: false,
-    recoversHealth: false,
-    healthRecoveryFactor: "",
-    recoversMana: false,
-    manaRecoveryFactor: "",
-    recoversStamina: false,
-    staminaRecoveryFactor: "",
-  };
-
-  return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={handleSubmit}
-      initialValues={initialFormState}
-    >
-      {({ handleSubmit, handleChange, values, touched, errors }) => {
-        console.log("values", values);
-
-        return (
-          <BasicModal
-            title={"Create New Item"}
-            submitButtonLabel="Create"
-            show={show}
-            handleClose={handleClose}
-            handleSubmit={handleSubmit}
-          >
-            <CreateItemForm
-              handleChange={handleChange}
-              values={values}
-              touched={touched}
-              errors={errors}
-            />
-          </BasicModal>
-        );
-      }}
-    </Formik>
-  );
+  switch (itemType) {
+    case ITEM_TYPE.CONSUMABLES:
+      return (
+        <CreateConsumableItemModal show={show} handleClose={handleClose} />
+      );
+    case ITEM_TYPE.ARMORS:
+      return (
+        <CreateConsumableItemModal show={show} handleClose={handleClose} />
+      );
+    case ITEM_TYPE.WEAPONS:
+      return (
+        <CreateConsumableItemModal show={show} handleClose={handleClose} />
+      );
+    default:
+      return <div>Select Item type</div>;
+  }
 }
